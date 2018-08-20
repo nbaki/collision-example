b3b3f5d9b9507c193ff583f4e75d7e584d1e662d
Rob Sturcke <robSturcke@users.noreply.github.com>
2018-08-20 12:33:29 -0400

    setup schedule apppointment form

---
+++ src/schedule-form.js
@@ -0,0 +1,18 @@
+// $( "form" ).on( "submit", function( event ) {
+//    event.preventDefault();
+//    console.log( $( this ).serialize() );
+//    var my_form = this;
+//    $.ajax({
+//       type: "POST",
+//       url: 'https://stewardfs.holmanautomotive.com/contact',
+//       data: $( this ).serialize(),
+//       success: function() {
+//           alert('Mail Sent');
+//       },
+//       complete: function() {
+//         my_form.reset();
+//       }
+//    });
+// });
+
+// Example code from StewardFS static site api
