b487e1b7be3c2d57a2f9af5d2d01aae08d0890c7
Rob Sturcke <robSturcke@users.noreply.github.com>
2018-08-29 10:02:48 -0400

    add navigation icon and header to location

--- src/pages/location.js
+++ src/pages/location.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col, Table } from 'reactstrap';
 import MapsImage from '../images/photos/mapshot.png';
 import '../stylesheets/location.css';
+import NavigationIcon from '../images/icons/navigation.svg';
 import TimeIcon from '../images/icons/time.svg';
 
 export default class Location extends React.Component {
@@ -23,6 +24,9 @@ export default class Location extends React.Component {
           <Container fluid>
             <Row>
               <Col md={4}>
+                <div className="text-center">
+                  <h2><img src={NavigationIcon} id="header-icon" alt="address" />Address</h2>
+                </div>
                 <div id="address-text">
                   <a href="https://goo.gl/maps/5sz3EfzHeuB2" target="_blank" rel="noopener noreferrer">
                     900 N Andrews Ave <br />
@@ -31,49 +35,45 @@ export default class Location extends React.Component {
                 </div>
                 <hr />
                 <div id="hours-wrap">
-                  <Row>
-                    <Col>
-                      <div className="text-center">
-                        <h2 id="hours-header"><img src={TimeIcon} id="time-icon" alt="hours" /> Hours</h2>
-                      </div>
-                      <Table>
-                        <tbody>
-                          <tr>
-                            <td>Monday</td>
-                            <td className="float-right">8:00 AM - 5:00 PM</td>
-                          </tr>
-                          <tr>
-                            <td>Tuesday</td>
-                            <td className="float-right">8:00 AM - 5:00 PM</td>
-                          </tr>
-                          <tr>
-                            <td>Wednesday</td>
-                            <td className="float-right">8:00 AM - 5:00 PM</td>
-                          </tr>
-                          <tr>
-                            <td>Thursday</td>
-                            <td className="float-right">8:00 AM - 5:00 PM</td>
-                          </tr>
-                          <tr>
-                            <td>Friday</td>
-                            <td className="float-right">8:00 AM - 5:00 PM</td>
-                          </tr>
-                          <tr>
-                            <td>Saturday</td>
-                            <td className="float-right">8:00 AM - 12:00 PM</td>
-                          </tr>
-                          <tr>
-                            <td>Sunday</td>
-                            <td className="float-right">Closed</td>
-                          </tr>
-                        </tbody>
-                      </Table>
-                    </Col>
-                  </Row>
+                  <div className="text-center">
+                    <h2 id="hours-header"><img src={TimeIcon} id="header-icon" alt="hours" /> Hours</h2>
+                  </div>
+                  <Table>
+                    <tbody>
+                      <tr>
+                        <td>Monday</td>
+                        <td className="float-right">8:00 AM - 5:00 PM</td>
+                      </tr>
+                      <tr>
+                        <td>Tuesday</td>
+                        <td className="float-right">8:00 AM - 5:00 PM</td>
+                      </tr>
+                      <tr>
+                        <td>Wednesday</td>
+                        <td className="float-right">8:00 AM - 5:00 PM</td>
+                      </tr>
+                      <tr>
+                        <td>Thursday</td>
+                        <td className="float-right">8:00 AM - 5:00 PM</td>
+                      </tr>
+                      <tr>
+                        <td>Friday</td>
+                        <td className="float-right">8:00 AM - 5:00 PM</td>
+                      </tr>
+                      <tr>
+                        <td>Saturday</td>
+                        <td className="float-right">8:00 AM - 12:00 PM</td>
+                      </tr>
+                      <tr>
+                        <td>Sunday</td>
+                        <td className="float-right">Closed</td>
+                      </tr>
+                    </tbody>
+                  </Table>
                 </div>
               </Col>
               <Col md={8}>
-                <div>
+                <div id="map-wrap">
                   <a href="https://goo.gl/maps/5sz3EfzHeuB2" target="_blank" rel="noopener noreferrer">
                     <img src={MapsImage} id="map" alt="Map" />
                   </a>
